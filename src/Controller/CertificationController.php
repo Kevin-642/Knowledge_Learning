24fe62b16b6fcaddee0aa938266867c2595e4326
Kevin-642 <166792276+Kevin-642@users.noreply.github.com>
2025-02-19 10:02:38 +0100

    Controller+Entity+Form

--- src/Controller/CertificationController.php
+++ src/Controller/CertificationController.php
@@ -2,17 +2,57 @@
 
 namespace App\Controller;
 
+use App\Repository\CertificationRepository;
+use App\Repository\ThemeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class CertificationController extends AbstractController
+class CertificationController extends AbstractController
 {
-    #[Route('/certification', name: 'app_certification')]
-    public function index(): Response
+    #[Route('/certifications', name: 'app_certifications')]
+    public function index(CertificationRepository $certificationRepository, ThemeRepository $themeRepository): Response
     {
+        //recupération de l'utilisateur
+        $user = $this->getUser();
+        //récupération des certification obtenue
+        $certifications = $certificationRepository->findBy(['user'=>$user]);
+        //Récupération de tous les themes ,cursus et leçon
+        $themes = $themeRepository->findAll();
+
+        $data=[];
+        foreach($themes as $theme){
+            $cursusList=[];
+            foreach($theme->getCursuses() as $cursus){
+                $lessonsList=[];
+                foreach($cursus->getLesson() as $lesson){
+                    foreach ($certifications as $certification){
+                        if($certification->getLesson() === $lesson){
+                            $lessonsList[]=[
+                                'lesson'=>$lesson,
+                                'obtainedAt'=>$certification->getObtainedAt(),
+                            ];
+                        }
+                    }
+                }
+                if(!empty($lessonsList)){
+                    $cursusList[]= [
+                        'cursus'=>$cursus,
+                        'lessonsList'=>$lessonsList
+                    ];
+                }
+            }
+            if (!empty($cursusList)) {
+                $data[] = [
+                    'theme' => $theme,
+                    'cursusList' => $cursusList,
+                ];
+            }
+        }
+
+
         return $this->render('certification/index.html.twig', [
-            'controller_name' => 'CertificationController',
+            'certifications'=>$data
         ]);
     }
 }
