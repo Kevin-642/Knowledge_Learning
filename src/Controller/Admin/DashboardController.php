0efe13c26abb03120113c730e5cfc0aabfadf768
Kevin-642 <166792276+Kevin-642@users.noreply.github.com>
2025-03-20 09:57:56 +0100

    correction

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Theme;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use App\Controller\Admin\ThemeCrudController;
 
 
 class DashboardController extends AbstractDashboardController
