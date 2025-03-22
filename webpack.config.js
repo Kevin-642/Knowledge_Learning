163096a15fe7d0459225b852fa759ab303dda445
Kevin-642 <166792276+Kevin-642@users.noreply.github.com>
2025-03-22 18:09:52 +0100

    correction stimulus

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  // Autres configurations de Webpack
+
+  resolve: {
+    alias: {
+      'stimulus': path.resolve(__dirname, 'node_modules/stimulus')
+    }
+  }
+};
+
 const Encore = require('@symfony/webpack-encore');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
