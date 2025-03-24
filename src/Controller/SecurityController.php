772ab3036977f6766a99470b732131302c63c10c
Kevin-642 <166792276+Kevin-642@users.noreply.github.com>
2025-03-24 11:09:17 +0100

    correctif

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -9,20 +9,25 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
+    #[Route(path: '/', name: 'app_home')]
+    public function home(): Response
+{
+    return $this->render('home/index.html.twig');
+}
     public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
-
-        // get the login error if there is one
-        $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
-        $lastUsername = $authenticationUtils->getLastUsername();
-
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+{
+    // Si l'utilisateur est déjà authentifié, redirige-le vers la page d'accueil
+    if ($this->getUser()) {
+        return $this->redirectToRoute('app_home');
     }
+
+    // Obtenez l'erreur d'authentification s'il y en a une
+    $error = $authenticationUtils->getLastAuthenticationError();
+    // Dernier nom d'utilisateur entré par l'utilisateur
+    $lastUsername = $authenticationUtils->getLastUsername();
+
+    return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+}
 
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): void
