a14d397a25e4ac5975109703a3229960d41a61bf
Kevin-642 <166792276+Kevin-642@users.noreply.github.com>
2025-03-28 09:44:08 +0100

    Ajout des assets compilés

---
+++ public/build/app.8153e758.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[524],{28:(s,e,o)=>{o(336);console.log("This log comes from assets/app.js - welcome to AssetMapper! 🎉")}},s=>{s.O(0,[336],(()=>{return e=28,s(s.s=e);var e}));s.O()}]);
