cedd43a507953edf49cad6eb7c62355cf0d312f4
Kevin <166792276+Kevin-642@users.noreply.github.com>
2026-08-26 12:36:55 +0200

    build: refresh production assets

---
+++ public/build/app.4f73f457.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[524],{28(s,e,o){o(336);console.log("This log comes from assets/app.js - welcome to AssetMapper! 🎉")}},s=>{s.O(0,[728],(()=>{return e=28,s(s.s=e);var e}));s.O()}]);
