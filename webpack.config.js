88ef25d3f113cd0c228ac7f8bd59b19fb014a2a9
Kevin <166792276+Kevin-642@users.noreply.github.com>
2026-08-26 12:32:06 +0200

    fix: update vulnerable dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const Encore = require('@symfony/webpack-encore');
+const Encore = require('@symfony/webpack-encore').default;
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -37,7 +37,6 @@ Encore
      * https://symfony.com/doc/current/frontend.html#adding-more-features
      */
     .cleanupOutputBeforeBuild()
-    .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
@@ -47,10 +46,12 @@ Encore
     //     config.plugins.push('@babel/a-babel-plugin');
     // })
 
-    // enables and configure @babel/preset-env polyfills
-    .configureBabelPresetEnv((config) => {
-        config.useBuiltIns = 'usage';
-        config.corejs = '3.38';
+    // Babel 8 delegates automatic core-js polyfills to this dedicated plugin.
+    .configureBabel((config) => {
+        config.plugins.push(['polyfill-corejs3', {
+            method: 'usage-global',
+            version: '3.48',
+        }]);
     })
 
     // enables Sass/SCSS support
